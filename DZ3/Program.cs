9442c04a051bff48b502bb19e7dd848a3bf0e55e
Evgenii <zvolev@mail.ru>
2022-10-19 23:57:40 +0300

    DZ3 v1.19.10

--- DZ3/Program.cs
+++ DZ3/Program.cs
@@ -6,16 +6,6 @@
 3 4 5
 3+5=8, 1+2+3=6, 8-6=2*/
 
-int InputArraySize(string message) // запрос, ввод  и проверка размера массива 
-{
-    Console.Write(message);
-    int length = Convert.ToInt32(Console.ReadLine());
-    if (length <= 0)
-        Console.Write("Недопустимый размер массива");
-    else
-        Console.WriteLine();
-    return length;
-}
 
 int Prompt(string message) // запрос и ввод значений
 {
@@ -24,46 +14,73 @@ int Prompt(string message) // запрос и ввод значений
     return number;
 }
 
-int[] CreateRandomArray(int argLength = 2, int argMin = 0, int argMax = 1) // создаем массив и наполняем его случайными значениями по заданным параметрам и выводим на экран
+int[,] FillRandomMatrix(int line, int column, int argMin, int argMax) // наполняем матрицу случайными значениями по заданным параметрам и выводим на экран
 {
-    int[] array = new int[argLength];
-
+    int[,] matrix = new int[line, column];
     Random rand = new Random();
-    for (int i = 0; i < argLength; i++)
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        array[i] = rand.Next(argMin, argMax);
-        //System.Console.WriteLine(array[i]);
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(argMin, argMax);
+        }
     }
-    return array;
+    return matrix;
 }
 
-int SumNumbersEvenElements(int[] array) //находит сумму элементов, стоящих на нечётных позициях
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]}\t ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int SumMaxLineMatrix(int[,] matr)
 {
     int sum = 0;
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < matr.GetLength(0); i++)
     {
-        if (i % 2 != 0)
+        int max = 0;
+        for (int j = 0; j < matr.GetLength(1); j++)
         {
-            sum += array[i];
+            if (max < matr[i, j]) max = matr[i, j];
         }
+        sum = sum + max;
     }
     return sum;
 }
 
-void PrintArray(int[] array) // выводим массив на печать
+int SumMinColumnMatrix(int[,] matr, int argMax)
 {
-    for (int i = 0; i < array.Length; i++)
+    int sum = 0;
+    for (int j = 0; j < matr.GetLength(1); j++)
     {
-        Console.Write($"{array[i]}, ");
+        int min = argMax;
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            if (matr[i, j] < min) min = matr[i, j];
+        }
+        sum = sum + min;
     }
+    return sum;
 }
 
-
-int len = InputArraySize("Укажите размер массива >- ");
-int min = Prompt("Укажите диапазон значений массива: от >- ");
+int line = Prompt("Укажите количество строк матрицы >- ");
+int column = Prompt("Укажите количество столбцов матрицы >- ");
+int min = Prompt("Укажите диапазон значений матрицы: от >- ");
 int max = Prompt("до (включительно) >- ");
-int[] array = CreateRandomArray(len, min, max - 1); // создаем массив по заданным параметрам
-PrintArray(array); // выводим массив на печать
-System.Console.WriteLine();
-System.Console.WriteLine($"сумму элементов, стоящих на нечётных позициях {SumNumbersEvenElements(array)}");
- // выводим количество нечётных чисел в массиве на печать
+
+int[,] matrix = FillRandomMatrix(line, column, min, max + 1);
+PrintMatrix(matrix);
+int sumMax = SumMaxLineMatrix(matrix);
+int sumMin = SumMinColumnMatrix(matrix, max);
+
+System.Console.WriteLine($"Сумма максимумов в строках {sumMax}");
+System.Console.WriteLine($"Сумма минимумов в столбцах {sumMin}");
+System.Console.WriteLine($"Разница между максимумами в строках и  минимумами в столбцах равна {sumMax - sumMin}");
